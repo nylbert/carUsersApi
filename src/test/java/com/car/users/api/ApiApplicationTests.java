8401d4663415ae038682612b6b1aeece192a4689
nylbert <nylbert.dias@gmail.com>
2024-02-16 20:23:51 -0300

    AT-01: Criar aplicacao spring boot

---
+++ src/test/java/com/car/users/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.users.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
